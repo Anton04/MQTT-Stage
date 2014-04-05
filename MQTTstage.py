5ad68cbf66f8dbb6f1a57d204d0955ea537e22d7
Anton04 <anton.gustafsson@tii.se>
2014-04-05 11:02:01 +0200

    Update MQTTstage.py

--- MQTTstage.py
+++ MQTTstage.py
@@ -1,6 +1,53 @@
 #!/usr/bin/python
 
+# By Anton Gustafsson
+# 2014-04-05
 
-#Check if the 
-def CheckDirectories():
+import os 
+
+
+class MQTTstage(path):
+  def __init__(self):
+    
+    if not (self.CheckDirectories(path))
+      raise Exception("Can't initialize directorires!")
+    self.basepath = path
+    
+  def CreateDirectory(self,path)
+        #Create directories
+    try:
+      os.stat(dir)
+    except:
+      try:
+        os.mkdir(dir)
+        return True
+      except:
+        return False 
+    
+    return True
+
+  #Make sure we gave the directories we need. 
+  def CheckDirectories(self,path):
+    
+    if path[-1] != "/":
+      path = path + "/"
+    
+    #Create directories
+    self.topics = path + "topics/"
+    self.actors = path + "actors/"
+    self.reactors = path + "reactors/"
+    
+    paths = [self.topics, self.actors, self.reactors]
+    
+    for directory in paths:
+      self.CreateDirectory(directory)
+      
+      
+
+if __name__ == '__main__':
+
+  #Use default path if no argument given. 
+  if len(sys.argv) == 0:
+    BASEPATH = ~/MQTT-Stage
+  
   
