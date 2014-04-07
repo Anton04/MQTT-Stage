d82d10c53eab8dbcfcf617756264ff4f4b6815f0
gustafsson <anton.gustafsson@tii.se>
2014-04-07 23:59:21 +0200

    Reactors now working

--- MQTTstage.py
+++ MQTTstage.py
@@ -6,8 +6,10 @@
 import os 
 import sys
 import subprocess
-from time import sleep 
+from time import sleep
+import time  
 import mosquitto
+import thread
 
 PORT = '/dev/ttyUSB0'
 PREFIX = "rfxcom"
@@ -41,10 +43,12 @@ class MQTTstage(mosquitto.Mosquitto):
     	self.username_pw_set(user,password)
 
     print "Connecting"
-    print self.connect(ip)
-    #self.subscribe(self.topic, 0)
+    self.connect(ip)
+    self.subscribe(self.topic, 0)
     self.on_connect = self.X_on_connect
     self.on_message = self.X_on_message
+
+    thread.start_new_thread(self.ControlLoop2,())
 
     return
     
@@ -60,26 +64,59 @@ class MQTTstage(mosquitto.Mosquitto):
     	print("RECIEVED MQTT MESSAGE: "+msg.topic + " " + str(msg.payload))
     	topics = msg.topic.split("/")
 
-	#Create directory
-	self.CreateDirectory(self.topics_path + msg.topic)    	
+	path = self.topics_path + msg.topic
 
+	if path[-1] != "/":
+	    path += "/"
+
+	#Create directory
+	if self.CheckPath(path):    	
+	   self.StartScrips(path,reactors=True,recursive=False,topic = msg.topic,message = str(msg.payload))
     #except:
 #	    print "Error when parsing incomming message."
     return
     
   def ControlLoop(self):
     # schedule the client loop to handle messages, etc.
-    print "Starting MQTT listener"
-    while True:
-    	self.StartActors()
-        sleep(0.1)
-        
-        self.loop(10000)
-        #Check if new actors has been added. 
 
+    while True:
+	#Check if new actors has been added. 
+    	self.StartActors()
+	self.UnloadRemovedScripts()
+	sleep(10)
+
+
+  def ControlLoop2(self):
+      while(True):
+      	self.loop()
+        sleep(0.1)
+
+  def CheckPath(self,path,Autocreate=True):
+
+    #Check full path
+    if self.CreateDirectory(path):
+       return True
+
+    #If we fail check dir by dir.
+    dirs = path.split("/")
+
+    subpath = ""
+
+    for dir in dirs:	
+
+        subpath += dir + "/"
+
+	print "Testing: " + subpath
+
+	if not self.CreateDirectory(subpath):
+	    return False
+		
+    return True
+        
     
   def CreateDirectory(self,path):
     #Create directories
+
     try:
       #print "Checking folder: " + path
       os.stat(path)
@@ -126,21 +163,26 @@ class MQTTstage(mosquitto.Mosquitto):
     return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
 
   def StartActors(self):
+    self.StartScrips(self.actors)
+
+  def StartScrips(self,path,reactors=False,recursive=False,topic = None,message = None):
   	
     #List all the files in the actors folder. 
-    tree = os.walk(self.actors)
+    tree = os.walk(path)
     (dirpath, dirnames, filenames) = tree.next()
-    
-    #ScriptsToRun = []
-    #print "Checking actor scripts!"	
     
     #Check for actor scripts. 
     for file in filenames:
     	#if file[-3:] != ".py"
     	if not self.is_exe(dirpath + "/" + file):
-    		continue
-	if file.find(".actor") == -1:
-		continue
+    	    continue
+	
+	#Is actor
+	is_actor = file.find(".actor") != -1
+	is_reactor = file.find(".reactor") != -1
+
+	if not (is_actor or (is_reactor and reactors)):
+	    continue
 
 	restart=False
 	
@@ -149,32 +191,55 @@ class MQTTstage(mosquitto.Mosquitto):
 	    #Yes. Check if running.
 	    if self.running[file][0].poll() is None:
 		#Still running
-		continue
+		if is_actor:
+		   continue
 	    else:
 	 	del self.running[file]
-		print file + " terminated. "
-		restart = True	
+		if is_actor:
+		    print file + " terminated. "
+		    restart = True	
 
-    	command = dirpath + file   # the shell command
+
+	#Add parameters.
+	param = " -h %s" % self.ip
+	
+	if topic:
+	    param += " -t %s" % topic
+	if message:
+	    param += " -m %s" % message
+	
+
+    	command = dirpath + file + param  # the shell command
+	commandlist = command.split(" ")
 
 	if restart:
-	    print "Restarting script: " + command
-	else:
-	    print "Starting script: " + command 
-    	process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+	    print "Restarting actor script: " + command
+	elif is_actor:
+	    print "Starting actor script: " + command
+ 	else:
+	    print "Triggering reactor script: " + command
+    	process = subprocess.Popen(commandlist, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
     	#output, error = process.communicate()
-	#print "stuck here"
     	    
     	pid_nr = process.pid
 	#TODO write pid to /running
     	    
 	self.running[file] = [process,command] 
-    	
+    
+    #TODO also check subfolders if recursive
+
+    return 
+
+  def UnloadRemovedScripts(self):
+	
     invalid = []
+
 
     #Unload removed scripts    
     for file in self.running:
-	if file not in filenames:
+        try:
+            os.stat(self.running[file][1].split(" ")[0])
+	except:
 	    print file + " removed. Killing process..."
 	    self.running[file][0].kill()
 	    invalid.append(file)
@@ -183,7 +248,7 @@ class MQTTstage(mosquitto.Mosquitto):
     for file in invalid:	
     	del self.running[file]
 
-    #TODO also check subfolders
+    #TODO also check subfolders if recursive
 
     
     return
