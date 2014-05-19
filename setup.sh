c565e9691a9ba17cdcc1adf5e8685a41eed212a3
gustafsson <anton.gustafsson@tii.se>
2014-05-19 13:13:21 +0200

    test

--- setup.sh
+++ setup.sh
@@ -2,13 +2,18 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+#Copy exe file
 sudo cp $DIR/MQTTstage.py /usr/bin/mqtt-stage
+
+#Copy upstart job
+#sudo cp $DIR/upstart.sh.conf /etc/init/mqtt-stage.conf
+
 
 mkdir ~/MQTT-Stage
 
-initctl reload-configuration
+#sudo initctl reload-configuration
 
-start mqtt-stage
+#start mqtt-stage
 
 
 
